f26b4d8863c59553eaaaa3b36e6ba53915241e55
Athul <athuldinesan005@gmail.com>
2020-12-02 14:07:40 +0530

    dwm: adjust font size and add few program keybindings
    * 10 font size seems big for my monitor and 9 seems too small so 9.5 it is !

--- config.def.h
+++ config.def.h
@@ -10,13 +10,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Meslo LG S:size=10",
-                                        "FontAwesome 5 Free:style=Regular:size=10",
-                                        "FontAwesome 5 Free:style=Solid:size=10",
-                                        "FontAwesome 5 Brands:style=Regular:size=10",
+static const char *fonts[]          = { "Meslo LG S:size=9.5",
+                                        "FontAwesome 5 Free:style=Regular:size=9.5",
+                                        "FontAwesome 5 Free:style=Solid:size=9.5",
+                                        "FontAwesome 5 Brands:style=Regular:size=9.5",
                                         "MaterialIcons:size=8",
 					};
-static const char dmenufont[]       = "Meslo LG S:size=9";
+static const char dmenufont[]       = "Meslo LG S:size=9.5";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -153,6 +153,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* Programs */
+	{ MODKEY,			XK_w,		spawn,		SHCMD("$BROWSER") },
+	{ MODKEY|ShiftMask,		XK_t,		spawn,		SHCMD("telegram-desktop") },
+	{ MODKEY,			XK_n,		spawn,		SHCMD("nautilus") },
 };
 
 /* button definitions */
