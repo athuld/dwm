d902f940db1cae7de08945fffe44255ba4ef450d
Athul Dinesan <athuldinesan5@gmail.com>
2021-07-03 11:38:59 +0530

    dwm: change font to fira code and also change dmenu colors
    * fira code seems to be my fav font nowadays
    * increase fontawesome icons fonts to match rest
    
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
@@ -11,19 +11,19 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Meslo LG S:size=9.5",
-                                        "FontAwesome 5 Free:style=Regular:size=9.5",
-                                        "FontAwesome 5 Free:style=Solid:size=9.5",
-                                        "FontAwesome 5 Brands:style=Regular:size=9.5",
+static const char *fonts[]          = { "Fira Code:size=9.5:style:Medium",
+                                        "FontAwesome 5 Free:style=Regular:size=10",
+                                        "FontAwesome 5 Free:style=Solid:size=10",
+                                        "FontAwesome 5 Brands:style=Regular:size=10",
                                         "MaterialIcons:size=8",
 					};
-static const char dmenufont[]       = "Meslo LG S:size=9.5";
+static const char dmenufont[]       = "Fira Code:size=10:style:Medium";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_dblue[]       = "#394065";
-static const char bordersel[]	      = "#008b8b";
+static const char col_dblue[]       = "#14213d";
+static const char col_red[]	        = "#e76f51";
 static const char col1[]            = "#ffffff";
 static const char col2[]            = "#ffffff";
 static const char col3[]            = "#ffffff";
@@ -51,7 +51,7 @@ static const char *colors[][3]      = {
 	[SchemeCol10] = { col10,     col_gray1, col_gray2 },
 	[SchemeCol11] = { col11,     col_gray1, col_gray2 },
 	[SchemeCol12] = { col12,     col_gray1, col_gray2 },
-	[SchemeSel]   = { col_gray4, col_dblue,  bordersel },
+	[SchemeSel]   = { col_gray4, col_dblue, col_dblue },
 };
 
 /* tagging */
@@ -98,7 +98,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", bordersel, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dblue, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
