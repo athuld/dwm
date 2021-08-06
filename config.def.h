8f2ea0cbfe64ebd426aa71ed8573329414e09720
Athul Dinesan <athuldinesan5@gmail.com>
2021-08-07 02:04:45 +0530

    Revert "dwm: fullscreen patch"
    This reverts commit 458ac5b07fc8030f4fbb26f6efc657eb54642742.

--- config.def.h
+++ config.def.h
@@ -117,7 +117,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	 	        XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ControlMask,		XK_comma,  cyclelayout,    {.i = -1 } },
